4344c99b1200f04c2b76b94a996f10f49d7f9cda
betweak12 <ys@betweak.com>
2021-08-12 22:47:30 +0900

    create Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Home(props) {
-  return <div>This is Home</div>;
+  return (
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <div className="jumbotron">
+            <h1>NFT MarketPlace</h1>
+            Account: {props.ethAccount || "Metamask연결이 필요합니다."}
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
--- src/pages/Query.js
+++ src/pages/Query.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 export default function Query(props) {
-  return <div>Query Page!</div>;
+  return (
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <div className="jumbotron">
+            <h1>QUERIES</h1>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h4>GET TOKEN OWNER</h4>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="Enter Token ID" />
+            <Form.Text className="text-muted">
+              {/* We'll never share your email with anyone else. */}
+            </Form.Text>
+            <Button size={"sm"}>GET Owner</Button>
+          </Form.Group>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 }

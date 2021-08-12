6d55b907bbea928cfebfd00da5d34d7eb4850690
betweak12 <ys@betweak.com>
2021-08-12 22:19:06 +0900

    nft market list

---
+++ src/components/MarketListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function MarketListItem(props) {
+  return (
+    <Link to={`/item/${props.tokenId}`}>
+      <Card style={{ width: "18rem", color: "black", textDecoration: "none" }}>
+        <Card.Img variant="top" src={props.imageSrc} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>{props.description}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
+  );
+}
--- src/pages/MarketList.js
+++ src/pages/MarketList.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import MarketListItem from "../components/MarketListItem";
 
 export default function MarketList(props) {
-  return <div>This is MarketList Page</div>;
+  return (
+    <Container style={{ marginTop: 120 }}>
+      <Row>
+        <Col xs={12} md={4}>
+          <MarketListItem />
+        </Col>
+      </Row>
+    </Container>
+  );
 }

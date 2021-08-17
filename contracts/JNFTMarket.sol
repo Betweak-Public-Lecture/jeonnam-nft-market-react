91f2f1403a454d3a4526b73cdceeea6fa397c340
betweak12 <ys@betweak.com>
2021-08-17 21:32:28 +0900

    create v1. NFTMarket

--- contracts/JNFTMarket.sol
+++ contracts/JNFTMarket.sol
@@ -54,11 +54,14 @@ contract JNFTMarket is Ownable {
      - ethereum을 listingPrice만큼 지급받아야 합니다.
      - _price는 0보단 커야합니다. (판매금액)
      - _itemIds를 1씩 증가시키면서 idToMarketItem의 id로써 사용되어야 합니다.
+
+     - 판매자가 호출
      */
     function createMarketItem(address _nftContract, uint256 _tokenId, uint256 _price) public payable {
         // 1. ethereum의 양이 listingPrice만큼 존재하는지
         require(msg.value == listingPrice);
         require(_price > 0);
+
         // 실제 NFT 토큰이 같은 JNFT Contract에 존재하기 때문에 
         // (현재 Tx를 발생시킨 account가 실제 NFT token의 주인인지는 JNFT Market에서는 확인이 불가능)
         // ==> 해당 컨트랙트에 접근해서 원소유주가 _tokenId의 주인이 맞는지 체크가 필요. 
@@ -71,20 +74,91 @@ contract JNFTMarket is Ownable {
         emit MarketItemCreated(_itemIds-1, _tokenId, _price, _nftContract, msg.sender, address(0));
     }
 
-
-
-
-    
-
-
-
     /**
-    [연습문제3]
-    createMarketSale(address _nftContract, uint256 _itemId) 을 정의하세요.
+    [연습문제3 - 구입 contract 정의하기]
+    createMarketSale(uint256 _itemId) 을 정의하세요.: 
     - 외부에서 접근이 가능해야합니다.
     - ethereum을 해당 MarketItem의 price만큼 지급받아야 합니다.
+    - 아직 거래가 체결되지 않았어야 합니다.
+
     - 실제 JNFT의 contract를 발생시켜야 합니다. (JNFT의 transferFrom 호출)
-    - itemsSold 를 증가시켜야 합니다.
-    - ethereum을 seller전달해야 합니다. 
+    - itemsSoldCount 를 증가시켜야 합니다.
+    - ethereum을 seller전달해야 합니다.
+
+    - 구매자가 호출 
      */
+    function createMarketSale(uint256 _itemId) public payable{
+        MarketItem storage item = idToMarketItem[_itemId];
+        require(msg.value == item.price); // 판매금액만킁 지불 받았는지
+        require(item.owner == address(0)); // 아직 거래되지 않았는지
+
+        //- 실제 JNFT의 contract를 발생시켜야 합니다. (JNFT의 transferFrom 호출)
+        IERC721(item.nftContract).transferFrom(item.seller, msg.sender, item.tokenId);
+        item.owner = msg.sender;
+        _itemSoldCount++;
+        // seller에게 이더리움 전송하기
+        payable(item.seller).transfer(item.price);
+    }
+
+  
+
+     /**
+     [연습문제4 - 팔리지 않은 모든 marketItem가져오기]
+     fetchUnsoldMarketItems() 정의.
+     - 외부에서 접근 가능해야합니다.
+     - 조회만 하는 함수이므로 가스 최적화를 위해 OO로 정의 하셔야 합니다.
+     - return 값은 MarketItem 배열이어야 합니다.
+     - 팔리지 않은 모든 item들을 return 해야 합니다.
+     */
+    function fetchUnsoldMarketItems() external view returns(MarketItem[] memory){
+        uint256 itemCount = _itemIds; // 전체 listing된 marketItemCount
+        uint256 unSoldItemCount = itemCount - _itemSoldCount;
+
+        // 팔리지 않은 모든 marketItem을 return
+        MarketItem[] memory unSoldMarketItems = new MarketItem[](unSoldItemCount);
+
+        // 전체 배열을 순회하면서 해당되는 MarketItem 찾아서 배정
+        uint256 count = 0;
+        for (uint256 i=0; i<itemCount; i++){
+            if(idToMarketItem[i].owner == address(0)){
+                unSoldMarketItems[count] = idToMarketItem[i];
+                count++;
+            }
+        }
+        return unSoldMarketItems;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     /**
+     [연습문제5 - fetchmarketItem가져오기]
+     fetchMarketItem(uint256 _itemId) 정의.
+     - 외부에서 접근 가능해야합니다.
+     - 조회만 하는 함수이므로 가스 최적화를 위해 OO로 정의 하셔야 합니다.
+     - return 값은 MarketItem이어야 합니다.
+     - _itemId에 해당하는 marketItem을 return 해야 합니다.
+      */
 }
